9aa90871016ce03a45ede7e34b1acf387a4e8769
r.karzhavin <r.karzhavin@yadro.com>
2024-07-18 15:29:00 +0300

    tmux new session

--- .scripts/tmux/new_session.sh
+++ .scripts/tmux/new_session.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 source ~/.profile_env
 
-NAME=$(echo '' | $FZF_PATH/fzf-tmux -p 15%,6% --info=hidden --no-separator --pointer=' ' --print-query | sed 's/\ /_/g')
+NAME=$(echo '' | $FZF_PATH/fzf-tmux -p 15%,6% --info=hidden --no-separator --pointer=' ' --print-query)
 [ $? -eq 130 ] && exit 0
 
 if [ "$NAME" == "" ]; then
     NAME=$(uuidgen)
 fi
 
+NAME=$(echo $NAME | sed 's/\ /_/g')
 TMUX='' tmux new-session -d -s $NAME -c "~/"
 if [[ -z "$TMUX" ]]; then
     tmux attach -t $NAME
