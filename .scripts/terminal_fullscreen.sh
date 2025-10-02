2529ced53a9b808ae91c67c1ce2f2c765a972899
r.karzhavin <r.karzhavin@yadro.com>
2025-10-02 11:27:12 +0300

    updated scripts

--- .scripts/terminal_fullscreen.sh
+++ .scripts/terminal_fullscreen.sh
@@ -13,17 +13,20 @@ if [ ${#SESSIONS[@]} -eq 0 ]; then
     tmux -2 new -d -s main
 fi
 
+## GNOME terminal
 # gnome-terminal --maximize -- /home/redpunk/.scripts/run_tmux.sh
+
+## Alacritty terminal
 # alacritty \
 #     -o 'window.decorations="none"' \
 #     -o 'window.startup_mode="Maximized"' \
-#     -t tmux & \
-#     -e tmux -2 attach -t main &
-# echo $! > $PID
+#     -t tmux -e tmux attach -t main &
 
+## Kitty terminal
+kitty \
+    --override=hide_window_decorations=yes \
+    --start-as=maximized \
+    --title tmux \
+    tmux attach -t main &
 
-alacritty \
-    -o 'window.decorations="none"' \
-    -o 'window.startup_mode="Maximized"' \
-    -t tmux -e tmux attach -t main &
 echo $! > $PID
